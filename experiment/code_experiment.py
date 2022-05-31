4aa0608d4d213a79c7c0650a885b9afa57423599
Catalinablom <75936151+Catalinablom@users.noreply.github.com>
2022-05-31 16:47:13 +0200

    werkend

--- experiment/code_experiment.py
+++ experiment/code_experiment.py
@@ -6,10 +6,12 @@ Created on Mon May 23 13:39:44 2022
 """
 
 from ast import Or
+# from selectors import EpollSelector
 from normalized_mutual_information import *
 import networkx as nx
 import networkx.algorithms.community as nx_comm
 import copy
+import community as community_louvain
 
 # https://networkx.org/documentation/stable/reference/generated/networkx.generators.community.LFR_benchmark_graph.html
 # exactly one of min_degree or average_degree must be specified.
@@ -17,10 +19,10 @@ import copy
 def LFR(n, t1, t2, mu, mincomsize, maxcomsize): #t1, t2 >1, 0<=mu<=1
     return nx.LFR_benchmark_graph(n, t1, t2, mu,min_degree=1 ,min_community = mincomsize, max_community = maxcomsize)
 
-G = LFR(20, 2.5, 2.5, 0.3,3, 7)
+G = LFR(40, 2.5, 2.5, 0.3,3, 7)
 nx.draw(G)
 communities = {frozenset(G.nodes[v]["community"]) for v in G}
-print(communities)
+print(communities) 
 
 # https://networkx.org/documentation/stable/reference/algorithms/generated/networkx.algorithms.community.quality.modularity.html
 #communities in shape [{0, 1, 2}, {3, 4, 5}]
@@ -30,14 +32,23 @@ def modularity(G, communities):
 # Implement Louvain for modularity, je moet louvain wel zelf implementeren, anders kun je geen andere objective functie hebben
 # Je wil modularity niet de functie pakken altijd, soms moet je alleen delta M hebben als je één node of één community verplaatst, dan is heel M uitrekenen veel langzamer dan delta M
 
-def Louvain_modularity_firstround(G):
-    
-    #step 1 : initliaze communities and modularity
-    communities = [{i} for i in range(G.number_of_nodes())]
-    
-    
-    #create dic which keeps track of key: node, value: position of community it belongs to
-    com_dic = generate_com_dic(communities)
+def calc_diff_M(original_G, com_from, com_to):
+    diff = 0
+    m = original_G.number_of_edges()
+    for i in com_from:
+        for j in com_to:
+            ki=  len(list(original_G.neighbors(i)))
+            kj=  len(list(original_G.neighbors(j)))
+            if original_G.has_edge(i,j):
+                
+                diff += 1 - (ki*kj) / (2*m)
+            else: 
+                diff += - (ki*kj) / (2*m)
+    diff = diff/ (2*m)
+    return diff
+            
+
+def Louvain_modularity_firstround(original_G, G, communities, com_dic):
     
     #step 2
     improvement = True
@@ -45,34 +56,48 @@ def Louvain_modularity_firstround(G):
         print("hier")
         test = []
         for node in list(G.nodes()):
-            max_diff_M = 0
-            M = modularity(G,communities)
-            for i in G.neighbors(node):
-                #find current and possible community
-                current_community = com_dic[node]
-                possible_community = com_dic[i]
-                
-                #make new possible partition
-                new_communities = copy.deepcopy(communities)
-                new_communities[current_community].remove(node)
-                new_communities[possible_community].add(node)
-                
-                #calculate modularity for new partition
-                new_M = modularity(G,new_communities)
-                diff_M = new_M - M
-                
-                #save option that gives highest increase in modularity
-                if diff_M > max_diff_M:
-                    best_option = possible_community
-                    max_diff_M = diff_M
             
-            if max_diff_M >0:        
-                #move node to best option
-                communities[current_community].remove(node)
-                communities[best_option].add(node)
+            max_diff_M = 0
+            # M = modularity(G,communities)
+            for i in G.neighbors(node):
+                if node != i: #snap neit waarom ie denkt dat een node een neighbour van zichzelf is maar prima
+                    
+                    # #make new possible partition
+                    # new_communities = copy.deepcopy(communities)
+                    # new_communities[current_community].remove(node)
+                    # new_communities[possible_community].add(node)
+
+                    #calculate modularity for new partition
+                    diff_M = calc_diff_M(original_G, communities[node], communities[i])
+                    
+                    #save option that gives highest increase in modularity
+                    if diff_M > max_diff_M:
+                        best_option = i
+                        max_diff_M = diff_M
+                        
+              
+            
+            if max_diff_M >0:  
+                print("max_diff_M", max_diff_M)      
+                # #move node to best option
+                # communities[current_community].remove(node)
+                # communities[best_option].add(node)
                 
-                #create dic which keeps track of key: node, value: position of community it belongs to
-                com_dic = generate_com_dic(communities)
+                #update dic which keeps track of key: node, value: position of community it belongs to
+                for j in communities[node]:
+                    com_dic[j] = best_option
+                
+                #move nodes to best option community
+                # print("best option", best_option)
+                # print("node", node)
+                if best_option != node:
+                    
+                    communities[best_option].update(communities[node])
+                    communities[node]={}
+                
+                #print statemnt
+                # print("updated to \n", communities)
+                
         
             if max_diff_M == 0 :
                 test.append(False)
@@ -93,14 +118,14 @@ def generate_com_dic(communities):
             
     return com_dic
     
-found_communities= Louvain_modularity_firstround(G)
-print("found ",found_communities)
-print("ground ",communities)
+
 
 #compresses each community into one node
 def induced_graph(com_dic, graph):
     "inspiration from https://github.com/taynaud/python-louvain/blob/master/community/community_louvain.py"
     
+    #nodecom_to_gencom keeps tracks of which nodes are actually in communities compressed to nodes
+    #is a dic with key: node name, value: set of nodes
     ret = nx.Graph()
     ret.add_nodes_from(com_dic.values())
 
@@ -111,17 +136,33 @@ def induced_graph(com_dic, graph):
 
     return ret
 
-def Louvain_mod(G):
+def Louvain_mod(original_G):
+    G = original_G
+    #step 1 : initliaze communities and modularity
+    communities = [{i} for i in range(G.number_of_nodes())]
+    
+    
+    #create dic which keeps track of key: node, value: position of community it belongs to
+    com_dic = generate_com_dic(communities)
+    
+    
+    
     improvement =1
     previous_mod= modularity(G, communities)
     while improvement > 0:
         #run first round
-        communities, com_dic = Louvain_modularity_firstround(G)
+        communities, com_dic = Louvain_modularity_firstround(original_G,G, communities, com_dic)
         
         #calculate new modularity
-        
-        improvement = 3
+        current_mod = modularity(original_G, communities)
+        improvement =  current_mod - previous_mod 
         G = induced_graph(com_dic,G)
+        previous_mod = current_mod
+    
+    
+    return communities, com_dic
+    
+    
     
     
 def communities_to_vector(G,communities):
@@ -130,11 +171,27 @@ def communities_to_vector(G,communities):
         for node in list(communities)[community]:
             t[node] = community
     return t
+
+found_communities, _= Louvain_mod(G)
+real_found_communities = partition = community_louvain.best_partition(G)
+print("found ",found_communities)
+print("ground ",communities)
+print("real_found_communities", real_found_communities)
             
 found_vector = communities_to_vector(G, found_communities)
 ground_vector = communities_to_vector(G, communities)
 
 print(norm_mutual_inf(found_vector,ground_vector))
+
+
+
+
+
+
+
+
+
+
 
 
 
