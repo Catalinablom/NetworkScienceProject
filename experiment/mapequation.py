59e86628a070aa7fefacf3eb2ba2e674e98da210
veerlevdh <veerle.vvdh@gmail.com>
2022-06-01 15:12:14 +0200

    werkt dit?
    werkt dit?

--- experiment/mapequation.py
+++ experiment/mapequation.py
@@ -10,16 +10,35 @@ import math
 import numpy as np
 from sklearn.preprocessing import normalize
 
-# TODO: vector q genereren
 
-def q(graph, communities): 
+def p_arrow(communities, p, i): # p_arrow as in paper, without the q term
+    result = 0
+    for node in communities[i]:
+        result += p[node]
+    return result
+
+
+def calculate_q(graph, communities, p): 
     'vector of probabilities to go out of a certain community'
+    q = []
     # lengte is aantal communities
-    return
+    def calculate_qi(alpha):
+        result = 0
+        for node in communities[alpha]:
+            edges_uit=0
+            for neighbour in graph.neighbors(node):
+                if neighbour not in communities[alpha]:
+                    edges_uit += 1
+        result += (p[node]/p_arrow(communities, p, alpha))*(edges_uit / graph.degree(node))
+    
+    for i in len(communities):
+        qi = calculate_qi(i)
+        q.append(qi)    
+    return q
 
     
 
-def p(graph): 
+def calculate_p(graph): 
     'Vector p of probabilities to be in a certain node'
 
     'Get adjacency matrix and normalize, such that columns sum to 1'
@@ -34,20 +53,17 @@ def p(graph):
     'Performs a max of 100 iterations and unless convergence is met earlier (within an error tolerance)'
     for i in range(100): 
         previous = p
-        p=np.matmul(A,p)
-        if np.allclose(previous,p, rtol = 1.e-5): #moet deze error tolerance kleiner?
+        p=np.matmul(A,p)        
+        print(p)
+        if np.allclose(previous,p, rtol = (1.e-5)/n): #moet deze error tolerance kleiner?
             return p.flatten().tolist()
     
     return p.flatten().tolist()
 
 
-def p_arrow(communities, p, i): # p_arrow as in paper, without the q term
-    result = 0
-    for node in communities[i]:
-        result += p[node]
-    return result
 
-def HQ(communities, q):
+
+def calculate_HQ(communities, q):
     
     def fraction(q, i):
         teller = q[i]
@@ -61,7 +77,7 @@ def HQ(communities, q):
     
     return -1*result
 
-def HPi(communities, q, p, i):
+def calculate_HPi(communities, q, p, i):
     
     p_sum = 0
     for b in communities[i]:
@@ -78,16 +94,20 @@ def HPi(communities, q, p, i):
     return result
 
 
-# graph = nx.gnp_random_graph(100, 0.3)
-#p = p(graph) # p wil je maar een keer berekenen, die is voor elke keuze van communities hetzelfde, en kost veel tijd
+def LFR(n, t1, t2, mu, mincomsize, maxcomsize): #t1, t2 >1, 0<=mu<=1
+    return nx.LFR_benchmark_graph(n, t1, t2, mu,min_degree=1 ,min_community = mincomsize, max_community = maxcomsize)
+
+graph = LFR(100, 2.5, 2.5, 0.1,3, 20)
+communities = {frozenset(graph.nodes[v]["community"]) for v in graph}
+p = calculate_p(graph) # p wil je maar een keer berekenen, die is voor elke keuze van communities hetzelfde, en kost veel tijd
 
 def map_equation(graph, communities, p):
-    q = q(graph, communities)
-    HQ = HQ(communities, q, p)
+    q = calculate_q(graph, communities, p)
+    HQ = calculate_HQ(communities, q, p)
     result = sum(q)*HQ
     for i in range(len(communities)):
-        p_arrow = p_arrow(communities, p, i)
-        HPi = HPi(communities, q, p, i)
-        result += (p_arrow*HPi)
+        p_a = p_arrow(communities, p, i)
+        HPi = calculate_HPi(communities, q, p, i)
+        result += (p_a*HPi)
     return result
         
