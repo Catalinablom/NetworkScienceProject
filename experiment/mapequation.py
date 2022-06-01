47d9ed134d1b65d261c7b1d38f549461a4a50a17
veerlevdh <veerle.vvdh@gmail.com>
2022-06-01 12:39:15 +0200

    Update mapequation.py

--- experiment/mapequation.py
+++ experiment/mapequation.py
@@ -7,20 +7,39 @@ Created on Tue May 31 14:26:01 2022
 
 import networkx as nx
 import math
+import numpy as np
+from sklearn.preprocessing import normalize
 
-# TODO: vectoren p en q genereren
+# TODO: vector q genereren
 
 def q(graph, communities): 
     'vector of probabilities to go out of a certain community'
     # lengte is aantal communities
     return
 
+    
 
 def p(graph): 
     'Vector p of probabilities to be in a certain node'
-    # met pagerank
-    # lengte is aantal nodes
-    return
+
+    'Get adjacency matrix and normalize, such that columns sum to 1'
+    A = nx.to_numpy_matrix(graph)
+    A = normalize(A, axis=0, norm='l1') 
+    
+    'Initialize p, equal probability to start in every node'
+    n = graph.number_of_nodes()
+    p = np.empty
+    p = np.full((n,1), 1/n)
+    
+    'Performs a max of 100 iterations and unless convergence is met earlier (within an error tolerance)'
+    for i in range(100): 
+        previous = p
+        p=np.matmul(A,p)
+        if np.allclose(previous,p, rtol = 1.e-5): #moet deze error tolerance kleiner?
+            return p.flatten().tolist()
+    
+    return p.flatten().tolist()
+
 
 def p_arrow(communities, p, i): # p_arrow as in paper, without the q term
     result = 0
@@ -58,7 +77,9 @@ def HPi(communities, q, p, i):
         
     return result
 
-p = p(graph) # p wil je maar een keer berekenen, die is voor elke keuze van communities hetzelfde, en kost veel tijd
+
+# graph = nx.gnp_random_graph(100, 0.3)
+#p = p(graph) # p wil je maar een keer berekenen, die is voor elke keuze van communities hetzelfde, en kost veel tijd
 
 def map_equation(graph, communities, p):
     q = q(graph, communities)
@@ -66,7 +87,7 @@ def map_equation(graph, communities, p):
     result = sum(q)*HQ
     for i in range(len(communities)):
         p_arrow = p_arrow(communities, p, i)
-        HPi(communities, q, p, i)
+        HPi = HPi(communities, q, p, i)
         result += (p_arrow*HPi)
     return result
         
