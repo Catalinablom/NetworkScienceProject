2493dedf0fed2b340f91d42c35e88e93b67d179e
Catalinablom <75936151+Catalinablom@users.noreply.github.com>
2022-06-06 21:14:15 +0200

    fugjh

--- experiment/main.py
+++ experiment/main.py
@@ -18,7 +18,7 @@ import random
 def init():
     # random.seed(25)
     " We have to find a combination of parameter values that always is able to produce a graph"
-    G = LFR(50, 2.5, 1.5, 0.2, 5, 20)
+    G = LFR(50, 2.5, 1.5, 0.1, 5, 20)
     # nx.draw(G)
     communities = {frozenset(G.nodes[v]["community"]) for v in G}
     p = calculate_p(G)
@@ -30,11 +30,14 @@ def main():
 
     found_communities_map, _= Louvain_map(G, p)
     found_communities_mod, _ = Louvain_mod(G)
+    real_mod_com = nx_comm.louvain_communities(G)
 
 
     #real_found_communities = nx_comm.louvain_communities(G)
     print("\nfound mod")
     print_communities(found_communities_mod)
+    print("\nreal mod")
+    print_communities(real_mod_com)
     print("\nfound map")
     print_communities(found_communities_map)
     print("\nground ")
@@ -45,10 +48,14 @@ def main():
     found_vector_map = communities_to_vector(G, found_communities_map)
     found_vector_mod = communities_to_vector(G, found_communities_mod)
     ground_vector = communities_to_vector(G, communities)
+    real_mod_vector = communities_to_vector(G,real_mod_com)
     #real_found_vector = communities_to_vector(G, real_found_communities)
 
     print("map eq results", norm_mutual_inf(found_vector_map,ground_vector))
     print("modularity result",norm_mutual_inf(found_vector_mod,ground_vector))
+    print("real modularity result",norm_mutual_inf(real_mod_vector,ground_vector))
+    
+    
 
     # print(norm_mutual_inf(real_found_vector,ground_vector))
 
