ff3b93ddb5a56194237cedbd9529f4a36fed80bb
Catalinablom <75936151+Catalinablom@users.noreply.github.com>
2022-06-08 15:28:18 +0200

    sdjk

--- experiment/main.py
+++ experiment/main.py
@@ -1,3 +1,4 @@
+from re import A
 from algo_map import *
 from algo_map import Louvain_map
 from algo_mod import *
@@ -10,7 +11,7 @@ import networkx.algorithms.community as nx_comm
 import copy
 import community as community_louvain
 import random
-import json
+import time
 
 def init():
     # random.seed(25)
@@ -23,9 +24,10 @@ def init():
     return G, communities, p
 
 def main():
-    mus = [0.05, 0.1,0.2,0.3]
-    comsizes = [(25, 100)]
-    num_runs = 1
+    mult = 20
+    mus = [0.05,0.1]
+    comsizes = [(mult, 5*mult), (mult, 6*mult)]
+    num_runs = 2
     results = {}
     
     for mu in mus:
@@ -33,9 +35,11 @@ def main():
             comsizeL, comsizeR = comsizes[comsize_num]
             map_results =[]
             mod_results = []
+            realmod_results = []
             for i in range(num_runs):
                 #generate graph
-                G, tries = LFR(200, 2, 1.05, mu, comsizeL, comsizeR, 0)
+                print("Creating graph")
+                G, tries = LFR(10*mult, 2, 1.05, mu, comsizeL, comsizeR, 0)
                 print("Graph created after ",tries," tries")
                 p = calculate_p(G)
                 
@@ -43,20 +47,29 @@ def main():
                 communities = {frozenset(G.nodes[v]["community"]) for v in G}
                 
                 #find diff communities
+                tic = time.perf_counter()
                 found_communities_map, _= Louvain_map(G, p)
+                toc = time.perf_counter()
+                print(f"map solution found in {toc - tic:0.4f} seconds")
                 found_communities_mod, _ = Louvain_mod(G)
+                real_found_communities = nx_comm.louvain_communities(G)
                 
                 #convert to vector
                 found_vector_map = communities_to_vector(G, found_communities_map)
                 found_vector_mod = communities_to_vector(G, found_communities_mod)
                 ground_vector = communities_to_vector(G, communities)
+                real_found = communities_to_vector(G,real_found_communities)
                 
                 #calculcate normalized mutual information
                 map_results.append(norm_mutual_inf(found_vector_map,ground_vector))
                 mod_results.append(norm_mutual_inf(found_vector_mod,ground_vector))
+                realmod_results.append(norm_mutual_inf(real_found,ground_vector))
                 
             results[(mu,comsize_num,"map")] = map_results
             results[(mu,comsize_num,"mod")] = mod_results
+            results[(mu,comsize_num,"realmod")] = realmod_results
+            
+
         
     save_results((mus, comsizes,num_runs),results)
 
@@ -90,3 +103,4 @@ def save_results(params,results):
 
 
 main()
+
