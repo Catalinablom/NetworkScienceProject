e757d23577669cf8ce8b0d09ae1f8c2d7f094fba
Catalinablom <75936151+Catalinablom@users.noreply.github.com>
2022-06-09 21:05:24 +0200

    push

--- experiment/main.py
+++ experiment/main.py
@@ -16,20 +16,23 @@ import time
 def init():
     # random.seed(25)
     " We have to find a combination of parameter values that always is able to produce a graph"
-    G, tries = LFR(50, 2, 1.05, 0.1, 10, 25, 0)
-    print(tries)
-    # nx.draw(G)
-    communities = {frozenset(G.nodes[v]["community"]) for v in G}
-    p = calculate_p(G)
-    return G, communities, p
+    # G, tries = LFR(50, 2, 1.05, 0.1, 10, 25, 0)
+    # print(tries)
+    # # nx.draw(G)
+    # communities = {frozenset(G.nodes[v]["community"]) for v in G}
+    # p = calculate_p(G)
+    # return G, communities, p
 
 def main():
-    mult = 10
     mus = [0.1]
     # mus=[0.1]
     # (3*mult,5*mult),(mult, 5*mult), (mult, 6*mult),
-    comsizes = [(10,50)]
-    num_runs = 1
+    a= 5
+    b = 25
+    c= 12
+    comsizes = [(a,b),(2*a,2*b),(3*a,3*b),(4*a,4*b), (a,2*b), (a,3*b),(a,4*b),(a+c,b+c),(a+2*c,b+2*c),(a+3*c,b+3*c)]
+    # comsizes = [(a,b)]
+    num_runs = 10
     results = {}
     
     for mu in mus:
@@ -38,22 +41,28 @@ def main():
             comsizeL, comsizeR = comsizes[comsize_num]
             map_results =[]
             mod_results = []
+            avg_com_sizes = []
             # realmod_results = []
             for i in range(num_runs):
                 #generate graph
                 print("Creating graph")
-                G, tries = LFR(100*mult, 2.8, 1.8, mu, comsizeL, comsizeR, 0)
+                tic = time.perf_counter()
+                G, tries = LFR(500, 2.8, 1.8, mu, comsizeL, comsizeR, 0)
                 print("Graph created after ",tries," tries")
                 p = calculate_p(G)
                 
-                #find ground communities
+                #find ground communities and avg community size
                 communities = {frozenset(G.nodes[v]["community"]) for v in G}
                 
+                num_com = len(communities)
+                sizes = 0
+                for community in communities:
+                    sizes += len(community)
+                avg_com_size = sizes/num_com
+                avg_com_sizes.append(avg_com_size)
+                    
                 #find diff communities
-                tic = time.perf_counter()
                 found_communities_map, _= Louvain_map(G, p)
-                toc = time.perf_counter()
-                print(f"map solution found in {toc - tic:0.4f} seconds")
                 found_communities_mod, _ = Louvain_mod(G)
                 # real_found_communities = nx_comm.louvain_communities(G)
                 
@@ -68,16 +77,17 @@ def main():
                 mod_results.append(norm_mutual_inf(found_vector_mod,ground_vector))
                 # realmod_results.append(norm_mutual_inf(real_found,ground_vector))
                 
+                toc = time.perf_counter()
+                print(f"Total run in in {toc - tic:0.4f} seconds")
+                
             results[(mu,comsize_num,"map")] = map_results
             results[(mu,comsize_num,"mod")] = mod_results
+            results[(mu,comsize_num,"gen")] = avg_com_sizes
             # results[(mu,comsize_num,"realmod")] = realmod_results
             
 
         
     save_results((mus, comsizes,num_runs),results)
-
-
-    "Er gaat wel nog wat fout met dat hij probeert een volle set te verplaatsen naar een lege, dat zou niet mogelijk moeten zijn"
 
 def save_results(params,results):
     mus, comsizes,num_runs = params
@@ -106,4 +116,3 @@ def save_results(params,results):
 
 random.seed(25)
 main()
-
